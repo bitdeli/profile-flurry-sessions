adde859276a0ac63c65c10d45e54cd2f8591ed47
Ville Tuulos <ville@bitdeli.com>
2013-01-08 05:34:34 +0000

    add initial __main__.py and config.json

---
+++ __main__.py
@@ -0,0 +1,18 @@
+from bitdeli import profile_events
+from bitdeli.protocol import params
+from bitdeli.chunkedlist import ChunkedList
+
+PROFILE_RETENTION = params()['plan']['retention-days']
+
+now = time.time() * 1000
+
+for profile, sessions in profile_events():
+    psessions = profile.get('sessions')
+    if psessions == None:
+        psessions = profile['sessions'] = ChunkedList()
+    psessions.push(sessions)
+    psessions.drop_chunks(lambda x: now - x['t'] <= PROFILE_RETENTION)
+    profile.set_expire(PROFILE_RETENTION)
+
+
+
